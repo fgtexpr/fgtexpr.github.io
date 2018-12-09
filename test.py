ce822c9fb7614195c21fdadb92088f928040ff17
evan <oams892@gmail.com>
2018-12-08 22:07:08 -0800

    tesssst

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ from browser import document, alert
 # bind event 'click' on button to function echo
 
 def echo(ev):
-    alert(document["zone"].value)
-    document["mybutton"].bind("click", echo)
+    alert("hello")
+
+document["mybutton"].bind("click", echo)
