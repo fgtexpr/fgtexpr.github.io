e36780a52b5113d95d79206a620fab15ad1cbe89
evan <oams892@gmail.com>
2018-12-19 10:39:23 -0800

    halp

--- website.py
+++ website.py
@@ -1,15 +1,12 @@
-from browser import document, html
+from browser import window, html, document
 
 elements = dict()
 
 def create_canvas():
-    canvas = html.CANVAS(width = 680, height = 480)
-    ctx = canvas.getContext("webgl")
+    canvas = html.CANVAS(id = "canv", width = 680, height = 480)
     if 'canvas' not in elements:
         elements['canvas'] = []
     elements['canvas'].append(canvas)
-    ctx.clearColor(1.0, 0.0, 0.0, 0.0, 1.0)
-    ctx.clear(ctx.COLOR_BUFFER_BIT)
     return canvas
 
 
