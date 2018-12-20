93f4fb981cbec84a1ce2c871109bdd807565935f
evan <oams892@gmail.com>
2018-12-20 12:30:37 -0800

    added test input

--- website.py
+++ website.py
@@ -6,15 +6,41 @@ elements = dict()
 
 graphic_elements = dict()
 
-def create_canvas():
+def create_canvas(name = None):
     if 'canvas' not in elements:
         elements['canvas'] = []
-    n_canvs = len(elements['canvas'])
-    canvas = html.CANVAS(id = "canv_" + str(n_canvs), width = 680, height = 480)
+    if name == None:
+        name = 'canv_' + str(len(elements['canvas']))
+    canvas = html.CANVAS(id = name, width = 680, height = 480)
     document <= canvas
-    canvas = window.Cango3D.new("canv_" + str(n_canvs))
+    canvas = window.Cango3D.new(name)
     elements['canvas'].append(canvas)
     return canvas
+
+def create_input(tpe, name = None):
+    if 'input' not in elements:
+        elements['input'] = []
+    if name == None:
+        name = 'input_' + str(len(elements['input']))
+
+    inp = html.INPUT(type = tpe, id = name)
+
+    document <= inp
+    elements['input'].append(inp)
+    
+    return inp
+
+def create_button(name = None):
+    if 'button' not in elements:
+        elements['button'] = []
+    if name == None:
+        name = 'button_' + str(len(elements['button']))
+    
+    btn = html.BUTTON(id = name)
+
+    document <= button
+    elements['button'].append(btn)
+    return btn
 
 def draw_plane(point, unit_normal, height = 30, color = "blue"):
     square = window.Shape3D.new(window.shapeDefs3D.square(height), 
@@ -23,7 +49,7 @@ def draw_plane(point, unit_normal, height = 30, color = "blue"):
 
     square.transform.translate(point[0], point[1], point[2])
     
-    theta_x = math.asin(unit_normal * Vector(0, 1, 0)
+    theta_x = math.asin(unit_normal * Vector(0, 1, 0))
     theta_y = math.asin(unit_normal * Vector(1, 0, 0))
     
     square.transform.rotate(0, 1, 0, math.degrees(theta_x))
@@ -36,6 +62,17 @@ def draw_plane(point, unit_normal, height = 30, color = "blue"):
     graphic_elements['square'].append(square)
     return square
 
+def action_draw_plane(ev):
+    n_in = list( map( float, document['normal_input'].value.split(',') ) )
+    point_in = list( map( float, document['point_input'].value.split(',')))
+
+    normal = Vector(n_in[0], n_in[1], n_in[2]).normalized()
+    pt = Vector(point_in[0], point_in[1], point_in[2]).normalized()
+
+    draw_plane(pt, normal, color = 'red')
+    
+    draw_elements(canv, graphic_elements)
+
 def draw_elements(canvas, elements, bg_color = 'aliceblue'):
     canvas.setWorldCoords3D(0, 0, 500)
     canvas.setPropertyDefault("backgroundColor", bg_color)
@@ -47,9 +84,14 @@ def draw_elements(canvas, elements, bg_color = 'aliceblue'):
 
 def setup():
     canv = create_canvas()
+    create_input('text', name = 'normal_input')
+    create_input('text', name = 'point_input')
+    create_button().bind('click', action_draw_plane)
     point = Vector(100, 200, 1)
     unit_normal = Vector(0.1, 0.2, 0.4).normalized()
     draw_plane(point, unit_normal)
     draw_elements(canv, graphic_elements)
+    
+
 
 setup()
