49f90491c948fb93ae96c31d45c39baca5df4eaf
evan <oams892@gmail.com>
2018-12-19 21:04:48 -0800

    fixed the normal rotation???

--- website.py
+++ website.py
@@ -46,7 +46,7 @@ def draw_elements(canvas, elements, bg_color = 'aliceblue'):
 def setup():
     canv = create_canvas()
     point = Vector(100, 200, 1)
-    unit_normal = Vector(0.5, 0.5, 0.1)
+    unit_normal = Vector(0.3, 0.3, 0.2)
     draw_plane(point, unit_normal)
     draw_elements(canv, graphic_elements)
 
