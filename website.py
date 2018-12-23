3e679b5f56a0472916d4f34e4d547ea3d55839b6
evan <oams892@gmail.com>
2018-12-23 11:40:46 -0800

    oooooooooof debug2

--- website.py
+++ website.py
@@ -54,7 +54,7 @@ def draw_plane(point, unit_normal, height = 30, color = "blue"):
     ur = ur_corner - ( (ur_corner * unit_normal) * unit_normal )
     lr = lr_corner - ( (lr_corner * unit_normal) * unit_normal )
     ll = ll_corner - ( (ll_corner * unit_normal) * unit_normal )
-
+    alert('snorg')
     inp = ['M', 2,-2,0, 'l',0,4,0, -4,0,0, 0,-4,0, 'z']
     alert('glorp')
     alert(inp)
