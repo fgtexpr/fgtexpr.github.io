6dd1c6937dabc903a7db2dba239f2b11c2bb8b09
evan <oams892@gmail.com>
2018-12-23 22:30:16 -0800

    hax?????????

--- website.py
+++ website.py
@@ -136,8 +136,8 @@ def setup():
     create_input('text', name = 'point_input')
     create_button().bind('click', action_draw_plane)
     draw_axis(0, 10, 0, 10, 0, 10)
-    alert('blammmmadoozzzzieee')
-    draw_patch(canv, Sphere(), us = 0, ue = 3.14, vs = 0, ve = 3.14, delta = 0.1, color = "red")
+    alert('bladoop')
+    draw_patch(canv, Sphere(), us = 0, ue = 6.28, vs = -1, ve = 1, delta = 0.1, color = "red")
     draw_elements(canv, graphic_elements)
     alert('tttt')
 
