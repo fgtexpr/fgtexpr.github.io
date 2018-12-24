ea7db7bc3be01bf6d3e4e367581698863feb314c
evan <oams892@gmail.com>
2018-12-23 21:35:53 -0800

    hax?

--- website.py
+++ website.py
@@ -112,7 +112,7 @@ def draw_axis(x_min, x_max, y_min, y_max, z_min, z_max):
         path[1 + i] = pair[0]*canv_options['scale']
         path[5 + i] = pair[1]*canv_options['scale']
         alert(path)
-        alert('blamo')
+        alert('blamooiee')
         path = window.Path3D.new(path, {'lineWidth': 5, 'strokeColor': 'red'})
         alert(path)
         graphic_elements['axis'].append(path)
