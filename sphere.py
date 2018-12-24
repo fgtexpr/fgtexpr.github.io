64253e99ad71d4379032b8740ef3260fd902a24c
evan <oams892@gmail.com>
2018-12-23 22:46:47 -0800

    hax?

--- sphere.py
+++ sphere.py
@@ -6,8 +6,8 @@ class Sphere(Patch):
         super().__init__()
     
     def call(self, point):
-        phi = point[0]
-        theta = point[1]
+        theta = point[0]
+        phi = point[1]
         x = math.cos(theta) * math.sin(phi)
         y = math.sin(phi) * math.sin(theta)
         z = math.cos(phi)
