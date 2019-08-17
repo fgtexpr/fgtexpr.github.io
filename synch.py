2ca708cca7886a3ac3b4382cc158d85357aea53d
Evan <evanwall@buffalo.edu>
2019-08-16 22:32:44 -0700

    refactor

--- synch.py
+++ synch.py
@@ -156,7 +156,7 @@ class Renderer:
         butt.bind("click", self.change_params_callback)
         self.container <= butt
 
-alert("a!???")
+alert("a!!!!")
 n = FixedLattice(exec("lambda x : 2 - x"), 0.01, 9)
 r = Renderer(n)
 r.draw_param_selector()
